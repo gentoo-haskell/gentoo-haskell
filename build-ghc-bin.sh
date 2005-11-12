7eb40a1b52d1fecda51fd93389cd630d83274803
Duncan Coutts <dcoutts@gentoo.org>
2005-11-12 19:49:42 +0000

    Add build-ghc-bin.sh script.

---
+++ build-ghc-bin.sh
@@ -0,0 +1,20 @@
+
+# we must be sure to build the binary package without using any flags
+# that would make the binary require a specific cpu model.
+
+# tuning for pentium4 is probably a reasonable choice for p4s and athlons
+export CFLAGS="-O2 -pipe"
+
+# currently we must disable doc and opengl support
+export USE="-doc -opengl -tetex -java"
+
+echo "you may also need to set PORTDIR_OVERLAY=\"\""
+echo "and also ACCEPT_KEYWORDS=\"~\${arch}\""
+echo "running:"
+echo "  USE=\"${USE}\" CFLAGS=\"${CFLAGS}\" emerge --buildpkgonly =ghc-$1"
+
+emerge --buildpkgonly =ghc-$1 || exit
+
+echo "created /usr/portage/packages/dev-lang/ghc-$1.tbz2"
+echo "to make the ghc-bin file just move it:"
+echo "mv /usr/portage/packages/dev-lang/ghc-$1 /usr/portage/distfiles/ghc-bin-$1.tbz2"
