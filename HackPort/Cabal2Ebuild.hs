174b365050696f7a34f792b3c52eabe510c50ec4
Lennart Kolmodin <kolmodin@dtek.chalmers.se>
2006-04-16 13:33:45 +0000

    Update header in Cabal2Ebuild

--- HackPort/Cabal2Ebuild.hs
+++ HackPort/Cabal2Ebuild.hs
@@ -173,7 +173,7 @@ standardLibs =
 
 showEBuild :: EBuild -> String
 showEBuild ebuild =
-  ss "# Copyright 1999-2005 Gentoo Foundation". nl.
+  ss "# Copyright 1999-2006 Gentoo Foundation". nl.
   ss "# Distributed under the terms of the GNU General Public License v2". nl.
   ss "# $Header:  $". nl.
   nl.
