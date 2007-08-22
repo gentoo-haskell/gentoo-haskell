88e99af70fd3ce48dd5fd2f523e4cec3513eab17
Lennart Kolmodin <kolmodin@dtek.chalmers.se>
2007-08-22 17:13:07 +0000

    Update binary build script

--- projects/build-ghc-bin.sh
+++ projects/build-ghc-bin.sh
@@ -1,4 +1,31 @@
 
+ghcpath() {
+    local p
+    p=$(type -P ghc)
+    if [[ -z $p ]]; then
+        p="(not found)"
+    fi
+    echo "GHC Path: $p"
+}
+
+if [[ "$#" == "0" ||  "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "Usage: $0 <ghc-version>"
+    echo
+    echo "Welcome to the delecate task of bootstrapping ghc."
+    echo "This script assumes that you have a working GHC version setup in"
+    echo "your path to be used for the bootstrapping."
+    echo
+    echo "It is recommended that you install an older version of"
+    echo "dev-lang/ghc, if it exists for your arch."
+    echo
+    echo "You may also need to set PORTDIR_OVERLAY=\"\""
+    echo "and also ACCEPT_KEYWORDS=\"~\${arch}\""
+
+    ghcpath
+
+    exit
+fi
+
 # we must be sure to build the binary package without using any flags
 # that would make the binary require a specific cpu model.
 
@@ -8,8 +35,9 @@ export CFLAGS="-O2 -pipe"
 # build with the documentation and enable bootstrapping
 export USE="-binary doc ghcbootstrap"
 
-echo "you may also need to set PORTDIR_OVERLAY=\"\""
+echo "You may also need to set PORTDIR_OVERLAY=\"\""
 echo "and also ACCEPT_KEYWORDS=\"~\${arch}\""
+ghcpath
 echo "running:"
 echo "  USE=\"${USE}\" CFLAGS=\"${CFLAGS}\" emerge --buildpkgonly =ghc-$1"
 
