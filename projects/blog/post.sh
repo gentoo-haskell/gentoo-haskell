f1d617ef9da8caad1a7188c79634272ceb516f66
Sergei Trofimovich <slyfox@gentoo.org>
2011-04-17 17:58:53 +0300

    projects/blog/post.sh: follow the changes merged to BL master
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- projects/blog/post.sh
+++ projects/blog/post.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# add something like the following to
+# your ~/.blogliterately/config
+#     blog gentoo-haskell
+#     url http://gentoohaskell.wordpress.com/xmlrpc.php
+#     api wordpress
+#     user "$WP_USER"
+#     password "$WP_PASSWORD"
+
 BlogLiterately \
     --hs-kate \
     --other-code-kate \
-    http://gentoohaskell.wordpress.com/xmlrpc.php \
-    "$WP_USER" \
-    "$WP_PASSWORD" \
+    gentoo-haskell \
     "$@"
