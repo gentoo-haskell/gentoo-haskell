07148eb5ce940a2238ba2086c647db0bbe75bc87
Florian Schmaus <flo@freakempire.de>
2011-11-11 11:39:26 +0100

    updated xmonad-log-applet ebuild added ebuild for xmonad-log-applet-2.0.0

--- gnome-extra/xmonad-log-applet/files/xmonad.hs
+++ gnome-extra/xmonad-log-applet/files/xmonad.hs
@@ -1,73 +1,60 @@
-import Control.OldException(catchDyn,try)
-import XMonad.Util.Run
-import Control.Concurrent
-import DBus
-import DBus.Connection
-import DBus.Message
-import System.Cmd
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
-import XMonad.Layout.Accordion
-import XMonad.Layout.Grid
-import XMonad.ManageHook
-import XMonad.Prompt
-import XMonad.Util.EZConfig
 
+import Control.OldException
 
-main = withConnection Session $ \ dbus -> do
-  getWellKnownName dbus
-  xmonad $ gnomeConfig { 
-    terminal = "xterm",
-    modMask = mod4Mask, -- set the mod key to the windows key
-    logHook  = dynamicLogWithPP (myPrettyPrinter dbus)
-  }
+import DBus
+import DBus.Connection
+import DBus.Message
 
+main :: IO ()
+main =  withConnection Session $ \dbus -> do
+    getWellKnownName dbus
+    xmonad $ gnomeConfig
+         { logHook = dynamicLogWithPP (prettyPrinter dbus)
+         }
 
--- This retry is really awkward, but sometimes DBus won't let us get our
--- name unless we retry a couple times.
+prettyPrinter :: Connection -> PP
+prettyPrinter dbus = defaultPP
+    { ppOutput   = dbusOutput dbus
+    , ppTitle    = pangoSanitize
+    , ppCurrent  = pangoColor "green" . wrap "[" "]" . pangoSanitize
+    , ppVisible  = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
+    , ppHidden   = const ""
+    , ppUrgent   = pangoColor "red"
+    , ppLayout   = const ""
+    , ppSep      = " "
+    }
+
 getWellKnownName :: Connection -> IO ()
-getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
-                                                getWellKnownName dbus)
- where
-  tryGetName = do
-    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
-    addArgs namereq [String "org.xmonad.Log", Word32 5]
-    sendWithReplyAndBlock dbus namereq 0
+getWellKnownName dbus = tryGetName `catchDyn` (\(DBus.Error _ _) -> getWellKnownName dbus)
+  where
+    tryGetName = do
+        namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+        addArgs namereq [String "org.xmonad.Log", Word32 5]
+        sendWithReplyAndBlock dbus namereq 0
+        return ()
+
+dbusOutput :: Connection -> String -> IO ()
+dbusOutput dbus str = do
+    msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" "Update"
+    addArgs msg [String ("<b>" ++ str ++ "</b>")]
+    -- If the send fails, ignore it.
+    send dbus msg 0 `catchDyn` (\(DBus.Error _ _) -> return 0)
     return ()
-
-
-myPrettyPrinter :: Connection -> PP
-myPrettyPrinter dbus = defaultPP {
-    ppOutput  = outputThroughDBus dbus
-  , ppTitle   = pangoColor "#003366" . shorten 50 . pangoSanitize
-  , ppCurrent = pangoColor "#006666" . wrap "[" "]" . pangoSanitize
-  , ppVisible = pangoColor "#663366" . wrap "(" ")" . pangoSanitize
-  , ppHidden  = wrap " " " "
-  , ppUrgent  = pangoColor "red"
-  }
-
-outputThroughDBus :: Connection -> String -> IO ()
-outputThroughDBus dbus str = do
-  let str' = "<span font=\"Terminus 9 Bold\">" ++ str ++ "</span>"
-  msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" "Update"
-  addArgs msg [String str']
-  send dbus msg 0 `catchDyn` (\ (DBus.Error _ _ ) -> return 0)
-  return ()
-
 
 pangoColor :: String -> String -> String
 pangoColor fg = wrap left right
- where
-  left  = "<span foreground=\"" ++ fg ++ "\">"
-  right = "</span>"
+  where
+    left  = "<span foreground=\"" ++ fg ++ "\">"
+    right = "</span>"
 
 pangoSanitize :: String -> String
 pangoSanitize = foldr sanitize ""
- where
-  sanitize '>'  acc = "&gt;" ++ acc
-  sanitize '<'  acc = "&lt;" ++ acc
-  sanitize '\"' acc = "&quot;" ++ acc
-  sanitize '&'  acc = "&amp;" ++ acc
-  sanitize x    acc = x:acc
-
+  where
+    sanitize '>'  xs = "&gt;" ++ xs
+    sanitize '<'  xs = "&lt;" ++ xs
+    sanitize '\"' xs = "&quot;" ++ xs
+    sanitize '&'  xs = "&amp;" ++ xs
+    sanitize x    xs = x:xs
