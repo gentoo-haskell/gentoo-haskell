9e11f46d18d16595758ea81d2a975839b3be2995
Mark Wright <gienah@gentoo.org>
2012-02-10 03:40:39 +1100

    Bump shakespeare to 0.10.3.1

---
+++ dev-haskell/shakespeare/files/shakespeare-0.10.3.1/test/ShakespeareBaseTest.hs
@@ -0,0 +1,53 @@
+module ShakespeareBaseTest (specs) where
+
+import Test.HUnit hiding (Test)
+import Test.Hspec.Monadic
+import Test.Hspec.HUnit ()
+
+import Text.ParserCombinators.Parsec (parse, ParseError, (<|>))
+import Text.Shakespeare.Base (parseVarString, parseUrlString, parseIntString)
+import Text.Shakespeare (defaultShakespeareSettings, ShakespeareSettings(..), PreConvert(..), PreConversion(..))
+
+-- run :: Text.Parsec.Prim.Parsec Text.Parsec.Pos.SourceName () c -> Text.Parsec.Pos.SourceName -> c
+
+specs :: Specs
+specs = describe "shakespeare-js" $ do
+  it "parseStrings" $ do
+    Right "%{var}" @=?  run varString "%{var}"
+    Right "@{url}" @=?  run urlString "@{url}"
+    Right "^{int}" @=?  run intString "^{int}"
+    Right "@{url}" @=?  run (varString <|> urlString <|> intString) "@{url} #{var}"
+
+  -- it "preFilter off" $ do
+  --   str <- preFilter defaultShakespeareSettings template
+  --   str @=? template
+
+  -- it "preFilter on" $ do
+  --   str <- preFilter preConversionSettings template
+  --   "unchanged `#{var}` `@{url}` `^{int}`" @=? str
+
+  -- it "preFilter ignore quotes" $ do
+  --   str <- preFilter preConversionSettings templateQuote
+  --   "unchanged '#{var}' `@{url}` '^{int}'" @=? str
+
+  where
+    varString = parseVarString '%'
+    urlString = parseUrlString '@' '?'
+    intString = parseIntString '^'
+
+    preConversionSettings = defaultShakespeareSettings {
+      preConversion = Just PreConvert {
+          preConvert = Id
+        , preEscapeBegin = "`"
+        , preEscapeEnd = "`"
+        , preEscapeIgnore = "'\""
+        }
+    }
+    template  = "unchanged #{var} @{url} ^{int}"
+    templateQuote = "unchanged '#{var}' @{url} '^{int}'"
+
+    run parser str = eShowErrors $ parse parser str str
+
+    eShowErrors :: Either ParseError c -> c
+    eShowErrors = either (error . show) id
+
