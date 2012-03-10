7c80c39e9ad4e08ab0bed0595f23a4feb2e3d95e
Sergei Trofimovich <slyfox@gentoo.org>
2012-03-10 17:02:47 +0300

    dev-haskell/math-functions: bum up to 0.1.1.1
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- dev-haskell/math-functions/files/math-functions-0.1.1.0/tests/tests.hs
+++
@@ -1,8 +0,0 @@
-import Test.Framework       (defaultMain)
-import qualified Tests.SpecFunctions
-import qualified Tests.Chebyshev
-
-main :: IO ()
-main = defaultMain [ Tests.SpecFunctions.tests
-                   , Tests.Chebyshev.tests
-                   ]
