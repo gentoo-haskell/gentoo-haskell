69c76cb8552c83e5e38ae710955eecf91b4851e0
Mark Wright <gienah@gentoo.org>
2012-07-09 20:28:19 +1000

    Bump doctest to 0.7.0

--- dev-haskell/doctest/files/doctest-0.6.1/test/interpreter/termination/test_program.hs
+++
@@ -1,7 +0,0 @@
-import qualified Interpreter
-
-main = Interpreter.withInterpreter [] $ \repl -> do
-  putStrLn "sleeping for 1 second..."
-  Interpreter.eval repl "import Control.Concurrent"
-  Interpreter.eval repl "threadDelay 1000000"
-  putStrLn "done"
