6751dd02d8eef61158756993f8116b3e35e5d5cf
Mark Wright <gienah@gentoo.org>
2012-08-18 15:28:56 +1000

    Update path-pieces tests for hspec 1.3

--- dev-haskell/path-pieces/files/path-pieces-0.1.1/test/main.hs
+++ dev-haskell/path-pieces/files/path-pieces-0.1.1/test/main.hs
@@ -22,49 +22,49 @@ main = hspecX specs
 
 specs :: Specs
 specs = do
-  describe "RoutePiece" $ do
-    prop "toRoutePiece <=> fromSinglePiece String" $ \(p::String) ->
-      case (fromRoutePiece . toSinglePiece) p of
+  describe "PathPiece" $ do
+    prop "toPathPiece <=> fromPathPiece String" $ \(p::String) ->
+      case (fromPathPiece . toPathPiece) p of
         Nothing -> null p
         Just pConverted -> p == pConverted
 
-    prop "toRoutePiece <=> fromSinglePiece String" $ \(p::T.Text) ->
-      case (fromRoutePiece . toSinglePiece) p of
+    prop "toPathPiece <=> fromPathPiece String" $ \(p::T.Text) ->
+      case (fromPathPiece . toPathPiece) p of
         Nothing -> T.null p
         Just pConverted -> p == pConverted
 
-    prop "toRoutePiece <=> fromSinglePiece String" $ \(p::Int) ->
-      case (fromRoutePiece . toSinglePiece) p of
+    prop "toPathPiece <=> fromPathPiece String" $ \(p::Int) ->
+      case (fromPathPiece . toPathPiece) p of
         Nothing -> p < 0
         Just pConverted -> p == pConverted
 
-  describe "RouteMultiPiece" $ do
-    prop "toRouteMultiPiece <=> fromMultiPiece String" $ \(p::[String]) ->
-      p == (fromJust . fromRouteMultiPiece . toMultiPiece) p
+  describe "PathMultiPiece" $ do
+    prop "toPathMultiPiece <=> fromPathMultiPiece String" $ \(p::[String]) ->
+      p == (fromJust . fromPathMultiPiece . toPathMultiPiece) p
 
-    prop "toRouteMultiPiece <=> fromMultiPiece String" $ \(p::[T.Text]) ->
-      p == (fromJust . fromRouteMultiPiece . toMultiPiece) p
+    prop "toPathMultiPiece <=> fromPathMultiPiece String" $ \(p::[T.Text]) ->
+      p == (fromJust . fromPathMultiPiece . toPathMultiPiece) p
 
 
   describe "SinglePiece" $ do
-    prop "toSinglePiece <=> fromSinglePiece String" $ \(p::String) ->
-      case (fromSinglePiece . toSinglePiece) p of
+    prop "toPathPiece <=> fromPathPiece String" $ \(p::String) ->
+      case (fromPathPiece . toPathPiece) p of
         Nothing -> null p
         Just pConverted -> p == pConverted
 
-    prop "toSinglePiece <=> fromSinglePiece String" $ \(p::T.Text) ->
-      case (fromSinglePiece . toSinglePiece) p of
+    prop "toPathPiece <=> fromPathPiece String" $ \(p::T.Text) ->
+      case (fromPathPiece . toPathPiece) p of
         Nothing -> T.null p
         Just pConverted -> p == pConverted
 
-    prop "toSinglePiece <=> fromSinglePiece String" $ \(p::Int) ->
-      case (fromSinglePiece . toSinglePiece) p of
+    prop "toPathPiece <=> fromPathPiece String" $ \(p::Int) ->
+      case (fromPathPiece . toPathPiece) p of
         Nothing -> p < 0
         Just pConverted -> p == pConverted
 
   describe "MultiPiece" $ do
-    prop "toMultiPiece <=> fromMultiPiece String" $ \(p::[String]) ->
-      p == (fromJust . fromMultiPiece . toMultiPiece) p
+    prop "toPathMultiPiece <=> fromPathMultiPiece String" $ \(p::[String]) ->
+      p == (fromJust . fromPathMultiPiece . toPathMultiPiece) p
 
-    prop "toMultiPiece <=> fromMultiPiece String" $ \(p::[T.Text]) ->
-      p == (fromJust . fromMultiPiece . toMultiPiece) p
+    prop "toPathMultiPiece <=> fromPathMultiPiece String" $ \(p::[T.Text]) ->
+      p == (fromJust . fromPathMultiPiece . toPathMultiPiece) p
