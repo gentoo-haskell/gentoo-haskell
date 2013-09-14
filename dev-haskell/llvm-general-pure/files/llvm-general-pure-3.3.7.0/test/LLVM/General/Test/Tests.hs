6accba06e3f7f2ccd93787ca9311c0bf3a8139a1
Sergei Trofimovich <slyfox@gentoo.org>
2013-09-14 11:36:06 +0300

    dev-haskell/llvm-general-pure: bump up to 3.3.8.1
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- dev-haskell/llvm-general-pure/files/llvm-general-pure-3.3.7.0/test/LLVM/General/Test/Tests.hs
+++
@@ -1,9 +0,0 @@
-module LLVM.General.Test.Tests where
-
-import Test.Framework
-
-import qualified LLVM.General.Test.PrettyPrint as PrettyPrint
-
-tests = testGroup "llvm-general" [
-    PrettyPrint.tests
-  ]
