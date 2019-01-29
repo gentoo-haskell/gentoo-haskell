gentoo-haskell/gentoo-haskell

commit 036f8f72e2fb0fcddacc21885e9014d4e03b8436

dev-haskell/reflex: bump up to 0.5
Package-Manager: Portage-2.3.59, Repoman-2.3.12
Signed-off-by: Jack Todaro <jackmtodaro@gmail.com>

dev-haskell/reflex/reflex-0.5.ebuild
@@ -3,14 +3,14 @@
 
 EAPI=7
 
-EGIT_REPO_URI="https://github.com/hololeap/reflex"
-inherit git-r3
+# ebuild generated by hackport 0.5.6.9999
 
 CABAL_FEATURES="lib profile haddock hoogle hscolour test-suite"
 inherit haskell-cabal
 
 DESCRIPTION="Higher-order Functional Reactive Programming"
 HOMEPAGE="https://reflex-frp.org"
+SRC_URI="mirror://hackage/packages/archive/${PN}/${PV}/${P}.tar.gz"
 
 LICENSE="BSD"
 SLOT="0/${PV}"
@@ -39,14 +39,15 @@ RDEPEND=">=dev-haskell/bifunctors-5.2:=[profile?] <dev-haskell/bifunctors-5.6:=[
 	>=dev-haskell/these-0.4:=[profile?] <dev-haskell/these-0.8:=[profile?]
 	>=dev-haskell/transformers-compat-0.3:=[profile?]
 	>=dev-haskell/unbounded-delays-0.1.0.0:=[profile?] <dev-haskell/unbounded-delays-0.2:=[profile?]
-	>=dev-lang/ghc-7.8.2:=
+	>=dev-lang/ghc-8.0.1:=
+	reflex-optimizer? ( dev-lang/ghc:=[profile?] )
 	template-haskell? ( >=dev-haskell/dependent-sum-0.3:=[profile?] <dev-haskell/dependent-sum-0.5:=[profile?]
 				>=dev-haskell/haskell-src-exts-1.16:=[profile?] <dev-haskell/haskell-src-exts-1.21:=[profile?]
 				>=dev-haskell/haskell-src-meta-0.6:=[profile?] <dev-haskell/haskell-src-meta-0.9:=[profile?] )
 	!template-haskell? ( >=dev-haskell/dependent-sum-0.4:=[profile?] <dev-haskell/dependent-sum-0.5:=[profile?] )
 "
 DEPEND="${RDEPEND}
-	>=dev-haskell/cabal-1.18.1.3
+	>=dev-haskell/cabal-1.24.0.0
 	test? ( dev-haskell/filemanip
 		dev-haskell/hlint
 		dev-haskell/split )

dev-haskell/reflex/Manifest
@@ -1,0 +1,1 @@
+DIST reflex-0.5.tar.gz 138056 BLAKE2B f168ffc69317e0556505e60e869a9e08a422f371bca7767e0b3c6a0d66fbdd1a55353a09827b63fb3737f520d5506443b2087d4128a3b0cae1e49a8eb1de1e17 SHA512 e82b2a6a505d6cfbba175fae9f182531db4cb6544c7c967fbb52e25154e3482a3e93f429b844e033cda94e2038e343bdd08551f4aadd4ad0f61555e921729ee3

dev-haskell/reflex/metadata.xml
@@ -6,8 +6,8 @@
 		<name>Gentoo Haskell</name>
 	</maintainer>
 	<use>
-		<flag name="use-reflex-optimizer">Use the GHC plugin Reflex.Optimizer on some of the modules in the package.  This is still experimental.</flag>
-		<flag name="use-template-haskell">Use template haskell to generate lenses</flag>
+		<flag name="reflex-optimizer">Use the GHC plugin Reflex.Optimizer on some of the modules in the package.  This is still experimental.</flag>
+		<flag name="template-haskell">Use template haskell to generate lenses</flag>
 		<flag name="debug-trace-events">Add instrumentation that outputs the stack trace of the definition of an event whenever it is subscribed to. Warning: It is very slow!</flag>
 		<flag name="fast-weak">Use the primitive implementation of FastWeak in GHCJS; note that this requires GHCJS to be built with FastWeak and FastWeakBag present in the RTS, which is not the default</flag>
 	</use>

commit 578ab5c8ac4e1a2a3c75195a4a18b330d5f3b818

dev-haskell/reflex: fix IUSE breakage introduced by 036f8f7
Package-Manager: Portage-2.3.59, Repoman-2.3.12
Signed-off-by: Jack Todaro <jackmtodaro@gmail.com>

dev-haskell/reflex/reflex-0.5.9999.ebuild
@@ -56,6 +56,6 @@ src_configure() {
 	haskell-cabal_src_configure \
 		$(cabal_flag debug-trace-events debug-trace-events) \
 		$(cabal_flag fast-weak fast-weak) \
-		$(cabal_flag reflex-optimizer reflex-optimizer) \
-		$(cabal_flag template-haskell template-haskell)
+		$(cabal_flag reflex-optimizer use-reflex-optimizer) \
+		$(cabal_flag template-haskell use-template-haskell)
 }

dev-haskell/reflex/reflex-0.5.ebuild
@@ -57,6 +57,6 @@ src_configure() {
 	haskell-cabal_src_configure \
 		$(cabal_flag debug-trace-events debug-trace-events) \
 		$(cabal_flag fast-weak fast-weak) \
-		$(cabal_flag reflex-optimizer reflex-optimizer) \
-		$(cabal_flag template-haskell template-haskell)
+		$(cabal_flag reflex-optimizer use-reflex-optimizer) \
+		$(cabal_flag template-haskell use-template-haskell)
 }