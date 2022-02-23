gentoo-haskell/gentoo-haskell

commit 3666843e07eece69f3867e2c1fcbc4087807bd96

dev-util/hoogle: Bump to 5.0.18.2-r2
Fix init scripts

Fixes: e112a7f17c7f84a6b2933fab064b2cd119a18ebd
Bug: https://github.com/gentoo-haskell/gentoo-haskell/issues/1240
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/hoogle/files/hoogle-generate.bash
@@ -8,7 +8,7 @@ output="$( "${CMD[@]}" )"
 stat="$?"
 
 if [ "${stat}" -eq 0 ]; then
-    count="$( echo "${output}" | grep '[0-9]\+/[0-9]\+' | wc -l )"
+    count="$( echo "${output}" | sed -rn -e 's/.*\[[0-9]+\/([0-9+])\].*/\1/p' | tail -1 )"
     echo "hoogle generate successful. ${count} packages added to index"
 else
     echo "${output}" | tail -3

dev-util/hoogle/files/hoogle-restart.service
@@ -1,0 +1,6 @@
+[Unit]
+Description=restart hoogle
+
+[Service]
+Type=oneshot
+ExecStart=/bin/systemctl restart hoogle

dev-util/hoogle/files/hoogle-restart.timer
@@ -2,7 +2,6 @@
 Description=Restart hoogle periodically
 
 [Timer]
-Unit=hoogle.service
 OnCalendar=daily
 
 [Install]

dev-util/hoogle/files/hoogle.initd
@@ -12,6 +12,7 @@ command_args="server --port ${HOOGLE_SERVER_PORT} --host ${HOOGLE_SERVER_HOST} $
 command_background="true"
 command_user="hoogle:hoogle"
 directory="/var/lib/hoogle"
+supervisor=supervise-daemon
 shell=/bin/sh
 su=/bin/su
 
@@ -21,6 +22,7 @@ start_pre() {
 
 generate() {
 	ebegin "Generating hoogle database"
+	HOOGLE_GENERATE_ARGS:="--local +RTS -N"
 	"$su" -c "/usr/libexec/hoogle-generate.bash ${HOOGLE_GENERATE_ARGS}" -s "$shell" - hoogle
 	eend $?
 }

dev-util/hoogle/files/hoogle.service
@@ -5,13 +5,13 @@ Requires=hoogle-generate.service
 After=hoogle-generate.service
 
 [Service]
-Type=oneshot
+Type=simple
 EnvironmentFile=-/etc/default/hoogle
 ExecStart=/usr/bin/hoogle server --port $HOOGLE_SERVER_PORT --host $HOOGLE_SERVER_HOST $HOOGLE_SERVER_EXTRA_ARGS
 User=hoogle
 Group=hoogle
 StateDirectory=hoogle
-#Restart=always
+Restart=always
 
 # Security settings for systemd running as root, optional but recommended to improve security. You
 # can disable individual settings if they cause problems for your use case. For more details, see

dev-util/hoogle/hoogle-5.0.18.2-r2.ebuild
@@ -75,8 +75,9 @@ src_install() {
 
 	if use systemd
 	then
-		systemd_dounit "${FILESDIR}/${PN}".{service,timer}
+		systemd_dounit "${FILESDIR}/${PN}.service"
 		systemd_dounit "${FILESDIR}/${PN}-generate.service"
+		systemd_dounit "${FILESDIR}/${PN}-restart".{service,timer}
 	else
 		insinto /etc/cron.daily
 		newins "${FILESDIR}/${PN}.cron" "${PN}-generate"

commit 6922e77311c951aa0a6f8bf5f3fc700b3bb06015

dev-util/hoogle: Fix typo
Fixes: 3666843e07eece69f3867e2c1fcbc4087807bd96
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/hoogle/files/hoogle-generate.bash
@@ -8,7 +8,7 @@ output="$( "${CMD[@]}" )"
 stat="$?"
 
 if [ "${stat}" -eq 0 ]; then
-    count="$( echo "${output}" | sed -rn -e 's/.*\[[0-9]+\/([0-9+])\].*/\1/p' | tail -1 )"
+    count="$( echo "${output}" | sed -rn -e 's/.*\[[0-9]+\/([0-9]+)\].*/\1/p' | tail -1 )"
     echo "hoogle generate successful. ${count} packages added to index"
 else
     echo "${output}" | tail -3