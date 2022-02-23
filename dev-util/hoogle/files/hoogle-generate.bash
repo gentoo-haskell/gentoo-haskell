gentoo-haskell/gentoo-haskell

commit e112a7f17c7f84a6b2933fab064b2cd119a18ebd

dev-util/hoogle: Bump to 5.0.18.2-r1
Fix init scripts

Bug: https://github.com/gentoo-haskell/gentoo-haskell/issues/1240
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/hoogle/files/hoogle-generate.bash
@@ -1,0 +1,16 @@
+#!/bin/bash
+
+HOOGLE=/usr/bin/hoogle
+CMD=($HOOGLE generate "$@")
+
+echo "Starting \"${CMD[@]}\""
+output="$( "${CMD[@]}" )"
+stat="$?"
+
+if [ "${stat}" -eq 0 ]; then
+    count="$( echo "${output}" | grep '[0-9]\+/[0-9]\+' | wc -l )"
+    echo "hoogle generate successful. ${count} packages added to index"
+else
+    echo "${output}" | tail -3
+    echo "${CMD[@]} failed with error code ${stat}"
+fi

dev-util/hoogle/files/hoogle.initd
@@ -1,5 +1,5 @@
 #!/sbin/openrc-run
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 extra_commands="generate"
@@ -12,7 +12,6 @@ command_args="server --port ${HOOGLE_SERVER_PORT} --host ${HOOGLE_SERVER_HOST} $
 command_background="true"
 command_user="hoogle:hoogle"
 directory="/var/lib/hoogle"
-supervisor=supervise-daemon
 shell=/bin/sh
 su=/bin/su
 
@@ -22,7 +21,6 @@ start_pre() {
 
 generate() {
 	ebegin "Generating hoogle database"
-	HOOGLE_GENERATE_ARGS:="--local"
-	"$su" -c "${command} generate ${HOOGLE_GENERATE_ARGS}" -s "$shell" - hoogle
+	"$su" -c "/usr/libexec/hoogle-generate.bash ${HOOGLE_GENERATE_ARGS}" -s "$shell" - hoogle
 	eend $?
 }

dev-util/hoogle/files/hoogle.service
@@ -5,13 +5,13 @@ Requires=hoogle-generate.service
 After=hoogle-generate.service
 
 [Service]
-Type=simple
+Type=oneshot
 EnvironmentFile=-/etc/default/hoogle
 ExecStart=/usr/bin/hoogle server --port $HOOGLE_SERVER_PORT --host $HOOGLE_SERVER_HOST $HOOGLE_SERVER_EXTRA_ARGS
 User=hoogle
 Group=hoogle
 StateDirectory=hoogle
-Restart=always
+#Restart=always
 
 # Security settings for systemd running as root, optional but recommended to improve security. You
 # can disable individual settings if they cause problems for your use case. For more details, see

dev-util/hoogle/files/hoogle-generate.service
@@ -5,7 +5,7 @@ Documentation=man:hoogle(1) https://github.com/ndmitchell/hoogle
 [Service]
 Type=oneshot
 EnvironmentFile=-/etc/default/hoogle
-ExecStart=/usr/bin/hoogle generate $HOOGLE_GENERATE_ARGS
+ExecStart=/usr/libexec/hoogle-generate.bash $HOOGLE_GENERATE_ARGS
 User=hoogle
 Group=hoogle
 StateDirectory=hoogle

dev-util/hoogle/files/hoogle.confd
@@ -3,5 +3,5 @@
 HOOGLE_SERVER_PORT="4006"
 HOOGLE_SERVER_HOST="localhost"
 
-HOOGLE_SERVER_EXTRA_ARGS="--local"
-HOOGLE_GENERATE_ARGS="--local"
+HOOGLE_SERVER_EXTRA_ARGS="--local +RTS -N"
+HOOGLE_GENERATE_ARGS="--local +RTS -N"

dev-util/hoogle/files/hoogle.cron
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rc-config generate hoogle
+/etc/init.d/hoogle restart

dev-util/hoogle/files/hoogle.timer
@@ -1,7 +1,8 @@
 [Unit]
-Description=Run hoogle generate
+Description=Restart hoogle periodically
 
 [Timer]
+Unit=hoogle.service
 OnCalendar=daily
 
 [Install]

dev-util/hoogle/hoogle-5.0.18.2-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -69,10 +69,14 @@ src_install() {
 
 	newinitd "${FILESDIR}/${PN}.initd" "${PN}"
 
+	insinto /usr/libexec
+	doins "${FILESDIR}/${PN}-generate.bash"
+	fperms 0755 "/usr/libexec/${PN}-generate.bash"
+
 	if use systemd
 	then
-		systemd_dounit "${FILESDIR}/${PN}.service"
-		systemd_dounit "${FILESDIR}/${PN}-generate".{service,timer}
+		systemd_dounit "${FILESDIR}/${PN}".{service,timer}
+		systemd_dounit "${FILESDIR}/${PN}-generate.service"
 	else
 		insinto /etc/cron.daily
 		newins "${FILESDIR}/${PN}.cron" "${PN}-generate"

commit 3666843e07eece69f3867e2c1fcbc4087807bd96

dev-util/hoogle: Bump to 5.0.18.2-r2
Fix init scripts

Fixes: e112a7f17c7f84a6b2933fab064b2cd119a18ebd
Bug: https://github.com/gentoo-haskell/gentoo-haskell/issues/1240
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/hoogle/files/hoogle-generate.bash
@@ -8,7 +8,7 @@ output="$( "${CMD[@]}" )"
 stat="$?"
 
 if [ "${stat}" -eq 0 ]; then
-    count="$( echo "${output}" | grep '[0-9]\+/[0-9]\+' | wc -l )"
+    count="$( echo "${output}" | sed -rn -e 's/.*\[[0-9]+\/([0-9+])\].*/\1/p' | tail -1 )"
     echo "hoogle generate successful. ${count} packages added to index"
 else
     echo "${output}" | tail -3

dev-util/hoogle/files/hoogle-restart.service
@@ -1,0 +1,6 @@
+[Unit]
+Description=restart hoogle
+
+[Service]
+Type=oneshot
+ExecStart=/bin/systemctl restart hoogle

dev-util/hoogle/files/hoogle-restart.timer
@@ -2,7 +2,6 @@
 Description=Restart hoogle periodically
 
 [Timer]
-Unit=hoogle.service
 OnCalendar=daily
 
 [Install]

dev-util/hoogle/files/hoogle.initd
@@ -12,6 +12,7 @@ command_args="server --port ${HOOGLE_SERVER_PORT} --host ${HOOGLE_SERVER_HOST} $
 command_background="true"
 command_user="hoogle:hoogle"
 directory="/var/lib/hoogle"
+supervisor=supervise-daemon
 shell=/bin/sh
 su=/bin/su
 
@@ -21,6 +22,7 @@ start_pre() {
 
 generate() {
 	ebegin "Generating hoogle database"
+	HOOGLE_GENERATE_ARGS:="--local +RTS -N"
 	"$su" -c "/usr/libexec/hoogle-generate.bash ${HOOGLE_GENERATE_ARGS}" -s "$shell" - hoogle
 	eend $?
 }

dev-util/hoogle/files/hoogle.service
@@ -5,13 +5,13 @@ Requires=hoogle-generate.service
 After=hoogle-generate.service
 
 [Service]
-Type=oneshot
+Type=simple
 EnvironmentFile=-/etc/default/hoogle
 ExecStart=/usr/bin/hoogle server --port $HOOGLE_SERVER_PORT --host $HOOGLE_SERVER_HOST $HOOGLE_SERVER_EXTRA_ARGS
 User=hoogle
 Group=hoogle
 StateDirectory=hoogle
-#Restart=always
+Restart=always
 
 # Security settings for systemd running as root, optional but recommended to improve security. You
 # can disable individual settings if they cause problems for your use case. For more details, see

dev-util/hoogle/hoogle-5.0.18.2-r2.ebuild
@@ -75,8 +75,9 @@ src_install() {
 
 	if use systemd
 	then
-		systemd_dounit "${FILESDIR}/${PN}".{service,timer}
+		systemd_dounit "${FILESDIR}/${PN}.service"
 		systemd_dounit "${FILESDIR}/${PN}-generate.service"
+		systemd_dounit "${FILESDIR}/${PN}-restart".{service,timer}
 	else
 		insinto /etc/cron.daily
 		newins "${FILESDIR}/${PN}.cron" "${PN}-generate"