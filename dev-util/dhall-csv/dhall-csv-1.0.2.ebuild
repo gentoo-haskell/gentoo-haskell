gentoo-haskell/gentoo-haskell

commit c6827e3df0f0898b53453cc2262c2c5a653247aa

dev-util/dhall-csv: Fix for recent eclass changes
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/dhall-csv/dhall-csv-1.0.2.ebuild
@@ -5,15 +5,16 @@ EAPI=8
 
 # ebuild generated by hackport 0.7.1.1.9999
 
-CABAL_FEATURES="lib profile haddock hoogle hscolour test-suite"
-inherit haskell-cabal
-
 # Tests depend on files that are only present on GitHub
 # This is the current package's release commit hash
 COMMIT="0ca7e4177ae315ab1593b8641ef5a2a62ecf1335"
-# SRC_URI="https://hackage.haskell.org/package/${P}/${P}.tar.gz"
-SRC_URI="https://github.com/dhall-lang/dhall-haskell/archive/${COMMIT}.tar.gz -> dhall-${COMMIT}.tar.gz"
 S="${WORKDIR}/dhall-haskell-${COMMIT}/${PN}"
+
+CABAL_FEATURES="lib profile haddock hoogle hscolour test-suite"
+inherit haskell-cabal
+
+# SRC_URI="https://hackage.haskell.org/package/${P}/${P}.tar.gz"
+SRC_URI="https://github.com/dhall-lang/dhall-haskell/archive/${COMMIT}.tar.gz -> dhall-haskell-${COMMIT}.tar.gz"
 
 HOMEPAGE="https://dhall-lang.org/
 	https://hackage.haskell.org/package/dhall-csv"

dev-util/dhall-csv/Manifest
@@ -1,1 +1,1 @@
-DIST dhall-0ca7e4177ae315ab1593b8641ef5a2a62ecf1335.tar.gz 892351 BLAKE2B e66e3676b9b22d40b46e05e0633de1b8ec369fd3d58ebf4d61469808b0d2a687f311a21c952ac52572572bc6079ef7cbdddb1584f2b02df419f6abd66d74b89c SHA512 d3ad1113d44ce86520d55f9fcf970d411c28e535e72b3af83787f1c2784fe441aeb1b5259d208d739269c0ddb48072c9d3edb23f3daa03cd8799265b0b6bc36a
+DIST dhall-haskell-0ca7e4177ae315ab1593b8641ef5a2a62ecf1335.tar.gz 892351 BLAKE2B e66e3676b9b22d40b46e05e0633de1b8ec369fd3d58ebf4d61469808b0d2a687f311a21c952ac52572572bc6079ef7cbdddb1584f2b02df419f6abd66d74b89c SHA512 d3ad1113d44ce86520d55f9fcf970d411c28e535e72b3af83787f1c2784fe441aeb1b5259d208d739269c0ddb48072c9d3edb23f3daa03cd8799265b0b6bc36a

commit 3e690fcce8f6ec715a41acd6d8f411e15509b2bb

dev-util/dhall-csv: Fixup
The $S variable must come _after_ the inherit, the $CABAL_FILE variable needs
to be set.

Needed after reverting 21931f370d60a28cd6a43f2ad513ab7110d7ed42

Fixes: c6827e3df0f0898b53453cc2262c2c5a653247aa
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/dhall-csv/dhall-csv-1.0.2.ebuild
@@ -5,16 +5,16 @@ EAPI=8
 
 # ebuild generated by hackport 0.7.1.1.9999
 
-# Tests depend on files that are only present on GitHub
-# This is the current package's release commit hash
-COMMIT="0ca7e4177ae315ab1593b8641ef5a2a62ecf1335"
-S="${WORKDIR}/dhall-haskell-${COMMIT}/${PN}"
-
 CABAL_FEATURES="lib profile haddock hoogle hscolour test-suite"
 inherit haskell-cabal
 
+# Tests depend on files that are only present on GitHub
+# This is the current package's release commit hash
+COMMIT="0ca7e4177ae315ab1593b8641ef5a2a62ecf1335"
 # SRC_URI="https://hackage.haskell.org/package/${P}/${P}.tar.gz"
 SRC_URI="https://github.com/dhall-lang/dhall-haskell/archive/${COMMIT}.tar.gz -> dhall-haskell-${COMMIT}.tar.gz"
+S="${WORKDIR}/dhall-haskell-${COMMIT}/${PN}"
+CABAL_FILE="${S}/${PN}.cabal"
 
 HOMEPAGE="https://dhall-lang.org/
 	https://hackage.haskell.org/package/dhall-csv"