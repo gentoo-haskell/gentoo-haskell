gentoo-haskell/gentoo-haskell

commit a42f8cd002a554ae8b3c4abe2ae08b1a26d474a3

dev-haskell/stack: Fix issue with $CABAL_FILE variable
Either $S needs to be set pre-inherit, or $CABAL_FILE needs to be set to
reflect the new value for $S.

Closes: https://github.com/gentoo-haskell/gentoo-haskell/issues/1281
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-haskell/stack/stack-2.7.4_pre20220125.ebuild
@@ -6,6 +6,14 @@ EAPI=8
 # ebuild generated by hackport 0.7.1.1.9999
 #hackport: flags: -disable-git-info,-hide-dependency-versions,-integration-tests,-static,-supported-build,-developer-mode
 
+# hololeap <hololeap@protonmail.com> (2022-01-25)
+# Use patch for now: https://github.com/commercialhaskell/stack/pull/5559
+# Rebased commit 336ab51 from brandon-leapyear/chinn/cabal onto aac15d0 from origin/master
+REPO_URI="https://github.com/hololeap/stack"
+COMMIT="9a0683679ad49982fcfb036f87c0f99a78916d2a"
+BRANCH="master"
+S="${WORKDIR}/${PN}-${COMMIT}"
+
 CABAL_FEATURES="lib profile haddock hoogle hscolour" # test-suite
 inherit haskell-cabal
 RESTRICT="test" # Requires internet access
@@ -13,14 +21,7 @@ RESTRICT="test" # Requires internet access
 DESCRIPTION="The Haskell Tool Stack"
 HOMEPAGE="https://haskellstack.org"
 
-# hololeap <hololeap@protonmail.com> (2022-01-25)
-# Use patch for now: https://github.com/commercialhaskell/stack/pull/5559
-# Rebased commit 336ab51 from brandon-leapyear/chinn/cabal onto aac15d0 from origin/master
-REPO_URI="https://github.com/hololeap/stack"
-COMMIT="9a0683679ad49982fcfb036f87c0f99a78916d2a"
-BRANCH="master"
 SRC_URI="${REPO_URI}/archive/${COMMIT}.tar.gz -> ${PN}-${COMMIT}.tar.gz"
-S="${WORKDIR}/${PN}-${COMMIT}"
 
 LICENSE="BSD"
 SLOT="0/${PV}"

commit 1ef8275d6ce7f77bea2a0aee5f942e89d70e9870

dev-haskell/stack: Fixup
The $S variable must come _after_ the inherit, the $CABAL_FILE variable needs
to be set.

Needed after reverting 21931f370d60a28cd6a43f2ad513ab7110d7ed42

Fixes: a42f8cd002a554ae8b3c4abe2ae08b1a26d474a3
Fixes: d091b5458b105ebb86856d0c9663145e34ef8dfd
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-haskell/stack/stack-2.7.4_pre20220125.ebuild
@@ -6,14 +6,6 @@ EAPI=8
 # ebuild generated by hackport 0.7.1.1.9999
 #hackport: flags: -disable-git-info,-hide-dependency-versions,-integration-tests,-static,-supported-build,-developer-mode
 
-# hololeap <hololeap@protonmail.com> (2022-01-25)
-# Use patch for now: https://github.com/commercialhaskell/stack/pull/5559
-# Rebased commit 336ab51 from brandon-leapyear/chinn/cabal onto aac15d0 from origin/master
-REPO_URI="https://github.com/hololeap/stack"
-COMMIT="9a0683679ad49982fcfb036f87c0f99a78916d2a"
-BRANCH="master"
-S="${WORKDIR}/${PN}-${COMMIT}"
-
 CABAL_FEATURES="lib profile haddock hoogle hscolour" # test-suite
 inherit haskell-cabal
 RESTRICT="test" # Requires internet access
@@ -21,7 +13,15 @@ RESTRICT="test" # Requires internet access
 DESCRIPTION="The Haskell Tool Stack"
 HOMEPAGE="https://haskellstack.org"
 
+# hololeap <hololeap@protonmail.com> (2022-01-25)
+# Use patch for now: https://github.com/commercialhaskell/stack/pull/5559
+# Rebased commit 336ab51 from brandon-leapyear/chinn/cabal onto aac15d0 from origin/master
+REPO_URI="https://github.com/hololeap/stack"
+COMMIT="9a0683679ad49982fcfb036f87c0f99a78916d2a"
+BRANCH="master"
 SRC_URI="${REPO_URI}/archive/${COMMIT}.tar.gz -> ${PN}-${COMMIT}.tar.gz"
+S="${WORKDIR}/${PN}-${COMMIT}"
+CABAL_FILE="${S}/${PN}.cabal"
 
 LICENSE="BSD"
 SLOT="0/${PV}"

dev-haskell/stack/stack-2.7.6_pre20220421.ebuild
@@ -6,21 +6,21 @@ EAPI=8
 # ebuild generated by hackport 0.7.1.1.9999
 #hackport: flags: -disable-git-info,-hide-dependency-versions,-integration-tests,-static,-supported-build,-developer-mode
 
-# hololeap <hololeap@protonmail.com> (2022-04-21)
-# Current released version (2.7.5) has issues with Cabal-3.4,
-# use latest upstream HEAD which has fixes.
-REPO_URI="https://github.com/commercialhaskell/stack"
-COMMIT="dcf7bb31815c7505e809a1707909832e02614501"
-BRANCH="master"
-S="${WORKDIR}/${PN}-${COMMIT}"
-
 CABAL_FEATURES="lib profile haddock hoogle hscolour test-suite"
 inherit haskell-cabal
 
 DESCRIPTION="The Haskell Tool Stack"
 HOMEPAGE="https://haskellstack.org"
 
+# hololeap <hololeap@protonmail.com> (2022-04-21)
+# Current released version (2.7.5) has issues with Cabal-3.4,
+# use latest upstream HEAD which has fixes.
+REPO_URI="https://github.com/commercialhaskell/stack"
+COMMIT="dcf7bb31815c7505e809a1707909832e02614501"
+BRANCH="master"
 SRC_URI="${REPO_URI}/archive/${COMMIT}.tar.gz -> ${PN}-${COMMIT}.tar.gz"
+S="${WORKDIR}/${PN}-${COMMIT}"
+CABAL_FILE="${S}/${PN}.cabal"
 
 LICENSE="BSD"
 SLOT="0/${PV}"