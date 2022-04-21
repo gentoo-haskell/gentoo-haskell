gentoo-haskell/gentoo-haskell

commit ec231b918f72844d22ba6c1c6ee5246680078668

dev-util/spago: Fix for recent eclass changes
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/spago/spago-0.20.6.ebuild
@@ -5,20 +5,21 @@ EAPI=8
 
 # ebuild generated by hackport 0.7.9999
 
+COMMIT="d79eeabde5abd2f6ddc8991459ff61beb8238ec1"
+S="${WORKDIR}/${PN}-${COMMIT}"
+
 CABAL_FEATURES="lib profile haddock hoogle hscolour" # test-suite
 inherit haskell-cabal
 RESTRICT="test" # Test fails to build on ghc-9.0.2 (2022-02-12)
 
 DESCRIPTION="PureScript package manager and build tool powered by Dhall and package-sets"
 HOMEPAGE="https://github.com/purescript/spago#readme"
-COMMIT="d79eeabde5abd2f6ddc8991459ff61beb8238ec1"
 SRC_URI="https://github.com/purescript/spago/archive/${COMMIT}.tar.gz -> ${PN}-${COMMIT}.tar.gz
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.10/purescript-docs-search -> ${PN}-purescript-docs-search-0.0.10
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.10/docs-search-app.js -> ${PN}-docs-search-app-0.0.10.js
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.11/purescript-docs-search -> ${PN}-purescript-docs-search-0.0.11
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.11/docs-search-app.js -> ${PN}-docs-search-app-0.0.11.js
 "
-S="${WORKDIR}/${PN}-${COMMIT}"
 
 LICENSE="BSD"
 SLOT="0/${PV}"

commit de4a787afd402df88f8152103a22bb1e82da3269

dev-util/spago: Fixup
The $S variable must come _after_ the inherit, the $CABAL_FILE variable needs
to be set.

Needed after reverting 21931f370d60a28cd6a43f2ad513ab7110d7ed42

Fixes: ec231b918f72844d22ba6c1c6ee5246680078668
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: hololeap <hololeap@users.noreply.github.com>

dev-util/spago/spago-0.20.6.ebuild
@@ -5,21 +5,21 @@ EAPI=8
 
 # ebuild generated by hackport 0.7.9999
 
-COMMIT="d79eeabde5abd2f6ddc8991459ff61beb8238ec1"
-S="${WORKDIR}/${PN}-${COMMIT}"
-
 CABAL_FEATURES="lib profile haddock hoogle hscolour" # test-suite
 inherit haskell-cabal
 RESTRICT="test" # Test fails to build on ghc-9.0.2 (2022-02-12)
 
 DESCRIPTION="PureScript package manager and build tool powered by Dhall and package-sets"
 HOMEPAGE="https://github.com/purescript/spago#readme"
+COMMIT="d79eeabde5abd2f6ddc8991459ff61beb8238ec1"
 SRC_URI="https://github.com/purescript/spago/archive/${COMMIT}.tar.gz -> ${PN}-${COMMIT}.tar.gz
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.10/purescript-docs-search -> ${PN}-purescript-docs-search-0.0.10
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.10/docs-search-app.js -> ${PN}-docs-search-app-0.0.10.js
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.11/purescript-docs-search -> ${PN}-purescript-docs-search-0.0.11
 	https://github.com/purescript/purescript-docs-search/releases/download/v0.0.11/docs-search-app.js -> ${PN}-docs-search-app-0.0.11.js
 "
+S="${WORKDIR}/${PN}-${COMMIT}"
+CABAL_FILE="${S}/${PN}.cabal"
 
 LICENSE="BSD"
 SLOT="0/${PV}"