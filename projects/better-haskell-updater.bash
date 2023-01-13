fa6e57d436ab4f6a292f35b35cc885c4c814bfe9
hololeap <hololeap@users.noreply.github.com>
2023-01-13 08:41:09 -0700

    projects: Rewrite better-haskell-updater.bash
    New version which has been working well for me
    
    Signed-off-by: hololeap <hololeap@users.noreply.github.com>

--- projects/better-haskell-updater.bash
+++ projects/better-haskell-updater.bash
@@ -3,24 +3,75 @@
 # Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-# Use this script like you would the `emerge` command, for instance:
-# > projects/better-haskell-updater.bash -DUu --complete-graph @world
-#
-# This will pull in packages from the underlying emerge command and also all
-# broken haskell packages. This should aid in rebuilding the necessary packages
-# on the first try.
+EIX=(
+	/usr/bin/eix
+	--installed-from-overlay haskell
+	--format '<installedversions:NAMESLOT>'
+	--pure-packages
+)
 
-HASKELL_UPDATER="/usr/sbin/haskell-updater"
-EMERGE="/usr/bin/emerge"
-EIX="/usr/bin/eix"
+XARGS=(
+	/usr/bin/xargs
+	-d'\n'
+)
 
-for e in $HASKELL_UPDATER $EMERGE $EIX; do
-    if [[ ! -x $e ]]; then
-        echo "Could not find needed executable: $e"
-        exit 1
-    fi
+EMERGE=(
+	/usr/bin/emerge
+	--oneshot
+	--complete-graph
+	--keep-going
+	--jobs=1
+	--quiet-build
+)
+
+my_printf() {
+	printf '"%s" ' "$@"
+}
+
+my_printf_other() {
+	echo "$@ "
+}
+
+my_printf_finish() {
+	echo
+}
+
+run_emerge() {
+	local after_pipe=(
+		"${XARGS[@]}"
+		"${EMERGE[@]}"
+		--update
+		--deep
+		"$@"
+	)
+	my_printf "${EIX[@]}"
+	my_printf_other
+	my_printf "${after_pipe[@]}"
+	my_printf_finish
+
+	"${EIX[@]}" | "${after_pipe[@]}"
+}
+
+run_haskell_updater() {
+	local args=(
+		haskell-updater
+		--
+		@preserved-rebuild
+	)
+	my_printf "${args[@]}"
+	my_printf_finish
+
+	"${args[@]}"
+}
+
+trap "exit 1" SIGINT
+
+emerge_result=1
+until [[ $emerge_result -eq 0 ]]
+do
+
+	run_emerge "$@"
+	emerge_result=$?
+
+	run_haskell_updater || exit 1
 done
-
-$EMERGE --complete-graph --oneshot --ask \
-    $($EMERGE --ignore-default-opts "$@" | $EIX --pipe --only-names) \
-    $($HASKELL_UPDATER --list-only)
