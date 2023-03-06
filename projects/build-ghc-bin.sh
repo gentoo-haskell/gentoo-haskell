3ca2c8d3756a8d3a5a617129ca801eb208359010
hololeap <hololeap@users.noreply.github.com>
2023-03-06 15:04:56 -0700

    projects/build-ghc-bin.sh: Migrate to gpkg binaries
    Make sure environment variables are set to use gpkg binaries and xz
    compression.
    
    Change messages to refer to '.gpkg.tar' files instead of '.tbz2' files.
    
    Various small tweaks and improvements
    
    Signed-off-by: hololeap <hololeap@users.noreply.github.com>

--- projects/build-ghc-bin.sh
+++ projects/build-ghc-bin.sh
@@ -23,8 +23,11 @@ if [[ "$#" == "0" ||  "$1" == "-h" || "$1" == "--help" ]]; then
 
     ghcpath
 
-    exit
+    exit 1
 fi
+
+GHC_VERSION="$1"
+shift
 
 # we must be sure to build the binary package without using any flags
 # that would make the binary require a specific cpu model.
@@ -43,12 +46,33 @@ export USE="-binary doc ghcbootstrap ghcmakebinary llvm ${USE}"
 # https://bugs.gentoo.org/667316
 export PORTAGE_COMPRESS=true
 
+# Use the new gpkg format, explicit xz compression with maximum compression
+# format (xz seems to be better than zstd for binary files)
+# Use as many threads as there are detected cores
+export BINPKG_FORMAT="gpkg"
+export BINPKG_COMPRESS="xz"
+export BINPKG_COMPRESS_FLAGS_XZ="-9 --threads=0"
+
+# This portage feature could affect the output filename
+export FEATURES="$(portageq envvar FEATURES) -binpkg-multi-instance"
+
+modified_envvars=(
+    "CFLAGS=\"${CFLAGS}\""
+    "USE=\"${USE}\""
+    "PORTAGE_COMPRESS=\"${PORTAGE_COMPRESS}\""
+    "BINPKG_FORMAT=\"${BINPKG_FORMAT}\""
+    "BINPKG_COMPRESS=\"${BINPKG_COMPRESS}\""
+    "BINPKG_COMRESS_FLAGS_XZ=\"${BINPKG_COMPRESS_FLAGS_XZ}\""
+)
+
 ghcpath
 echo "running:"
-echo "  USE=\"${USE}\" CFLAGS=\"${CFLAGS}\" emerge --buildpkgonly =ghc-$1"
+printf "%s " "${modified_envvars[@]}"
+echo "emerge --buildpkgonly ${@} =ghc-${GHC_VERSION}"
 
-emerge --buildpkgonly =ghc-$1 || exit
+emerge --buildpkgonly "${@}" =ghc-${GHC_VERSION} || exit 1
 
-echo "created $(portageq envvar PKGDIR)/dev-lang/ghc-$1.tbz2"
+echo "created $(portageq envvar PKGDIR)/dev-lang/ghc-${GHC_VERSION}.gpkg.tar"
 echo "to make the ghc-bin file just move it:"
-echo "mv $(portageq envvar PKGDIR)/dev-lang/ghc-$1.tbz2 $(portageq envvar DISTDIR)/ghc-bin-$1-$(portageq envvar CHOST).tbz2"
+echo "cp -v $(portageq envvar PKGDIR)/dev-lang/ghc-${GHC_VERSION}.gpkg.tar $(portageq envvar DISTDIR)/ghc-bin-${GHC_VERSION}-$(portageq envvar CHOST).gpkg.tar"
+echo "chmod a+r $(portageq envvar DISTDIR)/ghc-bin-${GHC_VERSION}-$(portageq envvar CHOST).gpkg.tar"
