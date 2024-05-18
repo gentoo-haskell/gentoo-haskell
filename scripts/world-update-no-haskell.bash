6427b4944564eb77a9da2dbf7e18c44b9e45cbda
hololeap <hololeap@protonmail.com>
2024-05-18 11:36:10 -0600

    scripts: Add world-update-no-haskell.bash
    Run `emerge -DUu @world`, but exclude any Haskell packages.
    
    Signed-off-by: hololeap <hololeap@protonmail.com>

---
+++ scripts/world-update-no-haskell.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Run `emerge -DUu @world`, but exclude any Haskell packages. This is
+# useful when you need to update your system but gentoo-haskell is
+# creating a mess, or when you simply want to skip Haskell upgrades.
+#
+# Usage: scripts/world-update-no-haskell.bash [emerge opts...]
+
+
+EMERGE=/usr/bin/emerge
+EIX_UPDATE=/usr/bin/eix-update
+EIX=(
+	/usr/bin/eix
+	--format '<installedversions:NAMESLOT>'
+	--pure-packages
+)
+HASKELL_UPDATER=(
+	/usr/bin/haskell-updater
+	--all
+	--list
+)
+HU_SED=(
+	/usr/bin/sed -r 's/(.*):.$/\1/'
+)
+
+not_haskell() {
+	comm -2 -3 \
+		<("${EIX[@]}" --world | sort -u) \
+		<("${HASKELL_UPDATER[@]}" | "${HU_SED[@]}" | sort -u)
+}
+
+$EIX_UPDATE
+
+COMMAND=(
+	$EMERGE -1DUu "$@" $(not_haskell)
+)
+
+printf "\"%s\" " "${COMMAND[@]}"; echo
+"${COMMAND[@]}"
